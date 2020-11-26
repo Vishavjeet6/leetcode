d224d90c8ce8e29ac5a6cd8edc102deaa629ac1e
Vishavjeet Thakur <vishavjeetsinghthakur@gmail.com>
2020-11-26 11:38:26 +0530

    Update ValidParentheses.java

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -3,22 +3,28 @@ package leetcode;
 import java.util.Stack;
 
 public class ValidParentheses {
-    public boolean isValid(String s) {
-    	Stack<Character> stack = new Stack<Character>();
-    	for(char c: s.toCharArray()) {
-    		if(c=='(') {
-    			stack.push(')');
-    		}
-    		else if(c=='{') {
-    			stack.push('}');
-    		}
-    		else if (c == '[') {
-    			stack.push(']');
-    		}
-    		else if(stack.isEmpty() || stack.pop() != c) {
-    			return false;
-    		}
-    	}
-		return stack.isEmpty();
+public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+//         for(char c: s.toCharArray()){
+            
+//         }
+        for(int i=0; i< s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(')');
+            }
+            else if(s.charAt(i) == '{'){
+                stack.push('}');
+            }
+            else if(s.charAt(i) == '['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop() != s.charAt(i)){
+                return false;
+            }
+            
+        }
+        return stack.isEmpty();
+        
     }
 }
