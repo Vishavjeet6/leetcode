a0cfa790f3563ba9431e0cce64cd680cb874226b
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2021-02-20 10:00:39 +0530

    adding dp

--- dynamicProgramming/UniquePathsII62.java
+++ dynamicProgramming/UniquePathsII62.java
@@ -2,40 +2,38 @@ package leetcode.dynamicProgramming;
 
 public class UniquePathsII62 {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		int m = obstacleGrid.length;
-	    int n = obstacleGrid[0].length;
-	    
-	    if(obstacleGrid[0][0]==1)return 0;
-	    
-	    
-	    int[][] dp = new int[m][n];
-	    
-	    for(int i=0;i<dp[0].length;i++){
-	       if(obstacleGrid[0][i]==0){
-	             dp[0][i] = 1;
-	        }else{
-	            break;
-	        }
-	    }
-	    for(int i=0;i<dp.length;i++){
-	        if(obstacleGrid[i][0]==0){
-	             dp[i][0] = 1;
-	        }else{
-	            break;
-	        }
-	       
-	    }
-	    
-	    for(int i=1;i<dp.length;i++){
-	        for(int j=1;j<dp[0].length;j++){
-	             if(obstacleGrid[i][j]==1){
-	                dp[i][j] = 0;
-	             }else{
-	                dp[i][j] = dp[i-1][j]+dp[i][j-1];  
-	             }
-	        }
-	    }
-	    
-	    return dp[m-1][n-1];
-	}   
+        if(obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] map = new int[m][n];
+        
+        for(int i = 0; i<m;i++){
+            if(obstacleGrid[i][0] == 0){
+                map[i][0] = 1;
+            }else{
+                break;
+            }        
+        }
+        
+        for(int j= 0;j<n;j++){
+            if(obstacleGrid[0][j] == 0){
+                map[0][j]=1;
+            }else{
+                break;
+            }    
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    map[i][j]=0;
+                }
+                else{
+                    map[i][j] = map[i-1][j]+map[i][j-1];
+                }
+                
+            }
+        }
+        return map[m-1][n-1];
+    } 
 }
