6db1c5e0a8d4c194467184ea0e55dc7f324ccf60
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2021-02-20 09:58:30 +0530

    adding dp questions

--- dynamicProgramming/LongestPalindromicSubstring5.java
+++ dynamicProgramming/LongestPalindromicSubstring5.java
@@ -31,23 +31,22 @@ public class LongestPalindromicSubstring5 {
         }
     
 //        second
-        public String longestPalindrome(String s) {
-            int n = s.length();
-     String res = null;
+        public String longestPalindromeDP(String s) {
+        	int n = s.length();
+        	String res = null;
+        	boolean[][] dp = new boolean[n][n];
        
-     boolean[][] dp = new boolean[n][n];
+		     for (int i = n - 1; i >= 0; i--) {
+		    	 for (int j = i; j < n; j++) {
+		    		 dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+		               
+		    		 if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+		    			 res = s.substring(i, j + 1);
+		    		 }
+		    	 }
+		     }
        
-     for (int i = n - 1; i >= 0; i--) {
-       for (int j = i; j < n; j++) {
-         dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
-               
-         if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
-           res = s.substring(i, j + 1);
-         }
-       }
-     }
-       
-     return res;
-       }
+		     return res;
+        }
 
 }
